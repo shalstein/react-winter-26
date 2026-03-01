03c0aa3b0eed7aef8f8a80ce0e4e6a2bd1b8b604
Sholom Steinmetz <sholomsteinmetz@gmail.com>
2026-03-01 16:26:18 -0500

    add profile component

--- src/App.jsx
+++ src/App.jsx
@@ -6,65 +6,9 @@ import MyGreeter from "./Greeter";
 import TodoList from "./TodoList";
 import ADefaultImport from "./TodoList";
 import Gallery from "./Gallery";
+import Profile from "./Profile";
 function App() {
-  const items = [
-    { name: "Cups", isCompleted: true },
-    { name: "Kiwi", isCompleted: true },
-    { name: "Chips", isCompleted: false },
-  ];
-  const shalom = [
-    {
-      name: "Shalom",
-      itemsToBuy: [
-        { name: "Coffee", isCompleted: false },
-        { name: "Tea", isCompleted: false },
-        { name: "Milk", isCompleted: false },
-      ],
-      theme: {
-        itemTheme: {
-          color: "purple",
-          fontWeight: "bold",
-          marginBottom: "8px",
-        },
-        listTheme: {
-          backgroundColor: "#f4f4f4",
-          padding: "20px",
-          borderRadius: "8px",
-          width: "300px",
-        },
-      },
-    },
-  ];
-  const jane = [
-    {
-      name: "Jane",
-      itemsToBuy: [
-        { name: "Cups", isCompleted: true },
-        { name: "Kiwi", isCompleted: true },
-        { name: "Chips", isCompleted: false },
-      ],
-      theme: {
-        itemTheme: {
-          color: "aqua",
-          fontWeight: "bold",
-          marginBottom: "8px",
-        },
-        listTheme: {
-          backgroundColor: "#f4f4f4",
-          padding: "20px",
-          borderRadius: "8px",
-          width: "300px",
-        },
-      },
-    },
-  ];
-  return (
-    <div>
-      <TodoList items={items} user={shalom} />
-      <TodoList items={items} user={jane} />
-      <Gallery />
-    </div>
-  );
+  return <Profile />;
 }
 
 export default App;
