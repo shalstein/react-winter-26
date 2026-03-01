5c36209425dba229c7aedb48204c9cf7787f358c
Sholom Steinmetz <sholomsteinmetz@gmail.com>
2026-03-01 16:16:35 -0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,48 +3,65 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import MyGreeter from "./Greeter";
-import { TodoList } from "./TodoList";
+import TodoList from "./TodoList";
 import ADefaultImport from "./TodoList";
 import Gallery from "./Gallery";
 function App() {
-  const shalom = {
-    name: "Shalom",
-    itemsToBuy: ["Coffee", "Tea", "Milk"],
-    theme: {
-      itemTheme: {
-        color: "purple",
-        fontWeight: "bold",
-        marginBottom: "8px",
-      },
-      listTheme: {
-        backgroundColor: "#f4f4f4",
-        padding: "20px",
-        borderRadius: "8px",
-        width: "300px",
-      },
-    },
-  };
-  const jane = {
-    name: "Jane",
-    itemsToBuy: ["Cups", "Kiwi", "Chips"],
-    theme: {
-      itemTheme: {
-        color: "aqua",
-        fontWeight: "bold",
-        marginBottom: "8px",
-      },
-      listTheme: {
-        backgroundColor: "#f4f4f4",
-        padding: "20px",
-        borderRadius: "8px",
-        width: "300px",
+  const items = [
+    { name: "Cups", isCompleted: true },
+    { name: "Kiwi", isCompleted: true },
+    { name: "Chips", isCompleted: false },
+  ];
+  const shalom = [
+    {
+      name: "Shalom",
+      itemsToBuy: [
+        { name: "Coffee", isCompleted: false },
+        { name: "Tea", isCompleted: false },
+        { name: "Milk", isCompleted: false },
+      ],
+      theme: {
+        itemTheme: {
+          color: "purple",
+          fontWeight: "bold",
+          marginBottom: "8px",
+        },
+        listTheme: {
+          backgroundColor: "#f4f4f4",
+          padding: "20px",
+          borderRadius: "8px",
+          width: "300px",
+        },
       },
     },
-  };
+  ];
+  const jane = [
+    {
+      name: "Jane",
+      itemsToBuy: [
+        { name: "Cups", isCompleted: true },
+        { name: "Kiwi", isCompleted: true },
+        { name: "Chips", isCompleted: false },
+      ],
+      theme: {
+        itemTheme: {
+          color: "aqua",
+          fontWeight: "bold",
+          marginBottom: "8px",
+        },
+        listTheme: {
+          backgroundColor: "#f4f4f4",
+          padding: "20px",
+          borderRadius: "8px",
+          width: "300px",
+        },
+      },
+    },
+  ];
   return (
     <div>
-      <TodoList user={shalom} />
-      <TodoList user={jane} />
+      <TodoList items={items} user={shalom} />
+      <TodoList items={items} user={jane} />
       <Gallery />
     </div>
   );
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,35 +1,45 @@
-function TodoList(props) {
-  const user = props.user;
+export default function TodoList(props) {
+  const user = props.user[0];
   return (
     <>
-      <h1 id="h1" className={user.name}>
-        {user.name}'s Shopping List
-      </h1>
+      <h1 id="h1">{user.name}'s Shopping List</h1>
       <ol style={user.theme.listTheme}>
         <li style={user.theme.itemTheme} id="itemid" className="item">
           {user.itemsToBuy[0]}
         </li>
-        <li style={user.theme.itemTheme}>{user.itemsToBuy[1]}</li>
-        <li style={user.theme.itemTheme}>{user.itemsToBuy[2]}</li>
+        <li style={user.theme.listTheme}>
+          {" "}
+          {user.itemsToBuy[0].isCompleted ? (
+            <del>user.itemsToBuy[0].name</del>
+          ) : (
+            user.itemsToBuy[0].name
+          )}
+        </li>
       </ol>
     </>
   );
 }
 
-function DrinkList() {
-  React.createElement("h1", null, "My Shoping List");
-  return React.createElement(
-    "ol",
-    null,
-    React.createElement("li", { className: "item", id: "itemid" }, "Coffee"),
-    React.createElement("li", null, "Tea"),
-    React.createElement("li", null, "Milk"),
-  );
-}
+// listTheme: {
+//   backgroundColor: "#f4f4f4",
+//   padding: "20px",
+//   borderRadius: "8px",
+//   width: "300px",
+// },
+// function DrinkList() {
+//   React.createElement("h1", null, "My Shoping List");
+//   return React.createElement(
+//     "ol",
+//     null,
+//     React.createElement("li", { className: "item", id: "itemid" }, "Coffee"),
+//     React.createElement("li", null, "Tea"),
+//     React.createElement("li", null, "Milk"),
+//   );
+// }
 
-function Example() {
-  return <h1>Example</h1>;
-}
+// function Example() {
+//   return <h1>Example</h1>;
+// }
 
-export { TodoList };
-export default Example;
+// export { TodoList };
+// export default Example;
