8963206b3343cbf039ac342fcc0e756cee0de597
Sholom Steinmetz <sholomsteinmetz@gmail.com>
2026-04-19 17:31:36 -0400

    add refs to our button so alert will work

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import OrderForm from "./OrderForm";
-
+import CounterButton from "./CounterButton";
+import { useRef, useState } from "react";
 function App() {
-  return <OrderForm />;
+  const [hidden, setHidden] = useState(false);
+  //let currentCount = 0;
+  const currentCountRef = useRef(0);
+  console.log("we rerendered app.jsx");
+  return (
+    <>
+      <button
+        onClick={() =>
+          alert("clicked " + ++currentCountRef.current + " times!")
+        }
+      >
+        Click me!
+      </button>
+      <CounterButton hidden={hidden} />
+      <br />
+      <label>Hide 2nd Button</label>
+      <input
+        type="checkbox"
+        checked={hidden}
+        onChange={() => setHidden(!hidden)}
+      />
+    </>
+  );
 }
 
 export default App;
