602c58d1a2efbe0ac6c2585032eb0ca07ed6173c
Sholom Steinmetz <sholomsteinmetz@gmail.com>
2026-05-03 15:14:44 -0400

    add clock component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import Stopwatch from "./Stopwatch";
 import MyVideo from "./MyVideo";
 import Form from "./Form";
+import { useEffect, useRef, useState } from "react";
+import Clock from "./Clock";
+
 function App() {
-  return (
-    <>
-      <Form />
-    </>
-  );
+  return <Clock time={"3:00 PM"} />;
 }
 
 export default App;
---
+++ src/Clock.jsx
@@ -0,0 +1,9 @@
+export default function Clock({ time }) {
+  return (
+    <>
+      {" "}
+      <h1>The time is now {time}</h1>
+      <input />
+    </>
+  );
+}
